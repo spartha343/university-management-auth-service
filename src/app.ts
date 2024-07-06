23b8b677ed3dbfccc1b22b8e72047876bca71877
partha <spartha343@gmail.com>
2024-07-06 17:34:42 +0600

    setup eslint, prettier, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
 //middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
 
-export default app;
+export default app
