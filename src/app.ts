aa0ff0e6a3abb7c355dc6ab5de7e6638e0732928
partha <spartha343@gmail.com>
2024-07-08 10:33:45 +0600

    [DONE] improve eslint setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import router from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -7,6 +8,9 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//Application routes
+app.use('/api/v1/users/', router)
 
 //testing
 app.get('/', (req: Request, res: Response) => {
